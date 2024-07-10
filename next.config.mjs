001a8b152a572b86ec333c8388f4ceec619ed6c6
Hanan Elhariri <halhariri@icon-creations.com>
2024-07-10 21:31:28 +0300

    fix: upload image

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ["html5book.ru", "pexels.com"]
+  }
+};
 
 export default nextConfig;
